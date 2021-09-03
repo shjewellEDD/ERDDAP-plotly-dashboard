2c146d0ac09c7194b4aeeb0cf5ff5b65af052853
shjewellEDD <scott.jewell@noaa.gov>
2021-09-03 14:16:20 -0700

    Added datatable. Got formatting looking good. Loading dongle added. Mean time to failure/error added to top of table.

--- diagnostic_dashboard.py
+++ diagnostic_dashboard.py
@@ -1,13 +1,11 @@
 '''
 TODO:
     Come up with a system to display failures
-    Mean time to
-        Error
-        Failure
     Show as a table
 '''
 
 import dash
+import dash_table
 import dash_design_kit as ddk  # Only available on Dash Enterprise
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
@@ -15,8 +13,6 @@ import plotly.express as px
 import pandas as pd
 import dash_html_components as dhtml
 from lxml import html
-import time
-import calendar
 from datetime import datetime
 import requests
 import datetime
@@ -136,14 +132,14 @@ class current_data:
         if not self.fail_state.empty:
         #     self.fail_state = False
         # else:
-           self.fail_state.loc[:, 'tdiff'] = self.fail_state.loc[:, 'datetime'].diff()
+           self.fail_state.loc[:, 'tdiff'] = (self.fail_state.loc[:, 'datetime'].diff()).astype('timedelta64[m]')
 
         self.error_state = self.eng_data[self.eng_data.loc[:, 'EC'].diff() != 0]
 
         if not self.error_state.empty:
         #     self.error_state = False
         # else:
-            self.error_state.loc[:, 'tdiff'] = self.error_state.loc[:, 'datetime'].diff()
+            self.error_state.loc[:, 'tdiff'] = (self.error_state.loc[:, 'datetime'].diff()).astype('timedelta64[m]')
 
         return self.error_state, self.fail_state
 
@@ -169,40 +165,92 @@ cur_set = current_data(base_url[0])
 data = cur_set.eng_data
 var_list = cur_set.vars
 
-app.layout = dhtml.Div([
-    dhtml.Div([
-        dhtml.Label(['Science Load']),
-        ddk.Card(width=100,
-                 children=[ddk.Graph(id='eng-graphic'#,
-                                     # figure=px.scatter(data,
-                                     #                   x='datetime',
-                                     #                   y='EC')
-                                     )
-                          ],
-                 )
-    ]),
-    dhtml.Div([
-        dhtml.Label(['Diagnostic Data']),
-        dcc.Dropdown(
-            id="select_set",
-            options=prawlers,
-            value=prawlers[0]['value']
-            #multi=True
-        ),
-        dhtml.Label(['Engineering Variables']),
-        dcc.Dropdown(
-            id="select_var",
-            options=var_list,
-            value=var_list[0]['value']
-        )
+graph_height = 300
 
+graph_config = {'modeBarButtonsToRemove' : ['hoverCompareCartesian','select2d', 'lasso2d'],
+                'doubleClick':  'reset+autosize', 'toImageButtonOptions': { 'height': None, 'width': None, },
+                'displaylogo': False}
+
+app.layout =  ddk.App([
+    ddk.Header([
+        ddk.Logo(src=app.get_asset_url('logo.png'), style={
+            'max-height':100,
+            'width': 'auto'
+        }),
+        ddk.Title('Prawler Engineering Diagnostic Dashboard'),
+        ddk.SectionTitle('', id='final_date'),
+        dhtml.Button('Refresh', style={'float':'right'}, id='refresh', n_clicks=0),
+    ]),
+dhtml.Div([
+    dhtml.Div([
+        ddk.Card(width=66,
+                 children=[dcc.Loading(id='load_loader',
+                                 children=[ddk.Graph(id='eng-graphic')]
+                                       ),
+                            dhtml.Label(['Prawler ID']),
+                            dcc.Dropdown(
+                                id="select_set",
+                                options=prawlers,
+                                value=prawlers[0]['value'],
+                                clearable=False
+                            ),
+                            dhtml.Label(['Engineering Variables']),
+                            dcc.Dropdown(
+                                id="select_var",
+                                options=var_list,
+                                value=var_list[0]['value'],
+                                clearable=False
+                            ),
+                ],
+            ),
+        ddk.Card(width=34,
+                 children=[dcc.Textarea(id='t_mean',
+                                        value='',
+                                        style={'width': '100%', 'height': 40},
+                                        ),
+                            dash_table.DataTable(id='table')])
+
+    ]),
+    ddk.Card(children=[
+        ddk.Block(width=6, children=
+        [
+            dhtml.Img(src='https://www.pmel.noaa.gov/sites/default/files/PMEL-meatball-logo-sm.png', height=100,
+                     width=100),
+
+        ]),
+        ddk.Block(width=90, children=[
+            dhtml.Div(children=[
+                dcc.Link('National Oceanic and Atmospheric Administration', href='https://www.noaa.gov/'),
+            ]),
+            dhtml.Div(children=[
+                dcc.Link('Pacific Marine Environmental Laboratory  |', href='https://www.pmel.noaa.gov/'),
+                dcc.Link('  Engineering', href='https://www.pmel.noaa.gov/edd/')
+            ]),
+            dhtml.Div(children=[
+                dcc.Link('oar.pmel.edd-webmaster@noaa.gov', href='mailto:oar.pmel.edd-webmaster@noaa.gov')
+            ]),
+            dhtml.Div(children=[
+                dcc.Link('DOC |', href='https://www.commerce.gov/'),
+                dcc.Link(' NOAA |', href='https://www.noaa.gov/'),
+                dcc.Link(' OAR |', href='https://www.research.noaa.gov/'),
+                dcc.Link(' PMEL |', href='https://www.pmel.noaa.gov/'),
+                dcc.Link(' Privacy Policy |', href='https://www.noaa.gov/disclaimer'),
+                dcc.Link(' Disclaimer |', href='https://www.noaa.gov/disclaimer'),
+                dcc.Link(' Accessibility', href='https://www.pmel.noaa.gov/accessibility')
+            ])
+        ])
     ])
+])
+
 ])
 
 # scientific data selection
 @app.callback(
     [Output('eng-graphic', 'figure'),
-     Output('select_var', 'options')],
+     Output('select_var', 'options'),
+     Output('table', 'data'),
+     Output('table', 'columns'),
+     Output('t_mean', 'value')],
     [Input('select_set', 'value'),
      Input('select_var', 'value')
      ])
@@ -215,17 +263,26 @@ def plot_diag(select_set, select_var):
 
     if select_var == 'TtFail':
         figure = px.scatter(cur_set.fail_state,
-                            y=cur_set.fail_state.loc[:, 'tdiff'].astype('timedelta64[m]'),
-                            x=cur_set.fail_state.loc[:, 'datetime'],
+                            y=cur_set.fail_state.loc[:, 'tdiff'],#.astype('timedelta64[m]'),
+                            x=cur_set.fail_state.loc[:, 'datetime']
                             )
+        t_mean = 'Mean time to fail: ' + str(round(cur_set.fail_state.loc[:, 'tdiff'].mean())) + ' minutes'
+        table_data = cur_set.fail_state.iloc[1:].to_dict('records')
+        columns = [{"name": 'Date', "id": 'datetime'},
+                   {'name': 'Time to Failure (minutes)', 'id': 'tdiff'}]
+
         figure.update_layout(
                             xaxis_title = "Time to Failure (minutes)",
                             yaxis_title = "Date"
                             )
     elif select_var == 'TtErr':
         figure = px.scatter(cur_set.err_state,
-                            y=cur_set.err_state.loc[:, 'tdiff'].astype('timedelta64[m]'),
+                            y=cur_set.err_state.loc[:, 'tdiff'],#.astype('timedelta64[m]'),
                             x=cur_set.err_state.loc[:, 'datetime'])
+        t_mean = 'Mean time to error: ' + str(round(cur_set.err_state.loc[:, 'tdiff'].mean())) + ' minutes'
+        table_data = cur_set.err_state.iloc[1:].to_dict('records')
+        columns = [{"name": 'Date', "id": 'datetime'},
+                   {'name': 'Time to Error (minutes)', 'id': 'tdiff'}]
         figure.update_layout(
                             yaxis_title = "Time to Error (minutes)",
                             xaxis_title = "Date"
@@ -235,6 +292,10 @@ def plot_diag(select_set, select_var):
                             y=cur_set.eng_data.loc[:, select_var],
                             x=cur_set.eng_data.loc[:, 'datetime']
                             )
+        t_mean = 'Mean time to error: ' + str(round(cur_set.err_state.loc[:, 'tdiff'].mean())) + ' minutes'
+        table_data = cur_set.err_state.iloc[1:].to_dict('records')
+        columns = [{"name": 'Date', "id": 'datetime'},
+                   {'name': 'Time to Error (minutes)', 'id': 'tdiff'}]
         figure.update_layout(xaxis_title = "Date",
                              yaxis_title = 'Error #')
     elif select_var == 'DD':
@@ -242,11 +303,15 @@ def plot_diag(select_set, select_var):
                             y=cur_set.eng_data.loc[:, select_var],
                             x=cur_set.eng_data.loc[:, 'datetime']
                             )
+        t_mean = 'Mean time to fail: ' + str(round(cur_set.fail_state.loc[:, 'tdiff'].mean())) + ' minutes'
+        table_data = cur_set.fail_state.iloc[1:].to_dict('records')
+        columns = [{"name": 'Date', "id": 'datetime'},
+                   {'name': 'Time to Failure (minutes)', 'id': 'tdiff'}]
         figure.update_layout(xaxis_title = "Date",
                              yaxis_title = 'Failure')
 
 
-    return figure, cur_set.vars
+    return figure, cur_set.vars, table_data, columns, t_mean
 
 if __name__ == '__main__':
     #app.run_server(host='0.0.0.0', port=8050, debug=True)
