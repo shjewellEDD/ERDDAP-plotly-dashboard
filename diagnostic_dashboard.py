f4b66a9c1fcaab2757cd3242c77c017147de2118
shjewellEDD <scott.jewell@noaa.gov>
2021-09-10 15:11:41 -0700

    Integrating dark themes and styling. Need to work on the best method.

--- diagnostic_dashboard.py
+++ diagnostic_dashboard.py
@@ -1,7 +1,8 @@
 '''
 TODO:
-    Come up with a system to display failures
-    Show as a table
+    Make labels human readable
+    Better plotting system, or at least a skiplist instead of if/then
+    Read in Seabird depth from the prawxxx data
 '''
 
 import dash
@@ -10,8 +11,10 @@ import dash_design_kit as ddk  # Only available on Dash Enterprise
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
-import pandas as pd
+import plotly.io as pio
 import dash_html_components as dhtml
+
+import pandas as pd
 from lxml import html
 from datetime import datetime
 import requests
@@ -29,19 +32,33 @@ int_indx = ['MM', 'PN', 'SR', 'IR', 'UL',  'LL', 'IP', 'ET', 'TS', 'EC', 'VM', '
 # user inputs
 base_url = ['http://eclipse.pmel.noaa.gov/rudics/TELO/RM12/',
             'http://eclipse.pmel.noaa.gov/rudics/TELO/RC12/',
-            'http://eclipse.pmel.noaa.gov/rudics/TELO/RC02/',
-            'https://data.pmel.noaa.gov/engineering/erddap/tabledap/prawler_eng_TELONAS2.csv'
+            'http://eclipse.pmel.noaa.gov/rudics/TELO/RC02/'#,
+            #'https://data.pmel.noaa.gov/engineering/erddap/tabledap/prawler_eng_TELONAS2.csv'
             ]
 
 prawlers = [{'label':   'RM12', 'value':    'http://eclipse.pmel.noaa.gov/rudics/TELO/RM12/'},
             {'label':   'RC02', 'value':    'http://eclipse.pmel.noaa.gov/rudics/TELO/RC02/'},
-            {'label':   'RC12', 'value':    'http://eclipse.pmel.noaa.gov/rudics/TELO/RC12/'},
-            {'label':   'TELONAS2', 'value': 'https://data.pmel.noaa.gov/engineering/erddap/tabledap/prawler_eng_TELONAS2.csv'}]
+            {'label':   'RC12', 'value':    'http://eclipse.pmel.noaa.gov/rudics/TELO/RC12/'}#,
+            #{'label':   'TELONAS2', 'value': 'https://data.pmel.noaa.gov/engineering/erddap/tabledap/prawler_eng_TELONAS2.csv'}
+            ]
 
 rudic_sets = {'RM12':   'http://eclipse.pmel.noaa.gov/rudics/TELO/RM12/',
               'RC02':   'http://eclipse.pmel.noaa.gov/rudics/TELO/RC02/',
               'RC12':   'http://eclipse.pmel.noaa.gov/rudics/TELO/RC12/',
-              'TELONAS2':   'https://data.pmel.noaa.gov/engineering/erddap/tabledap/prawler_eng_TELONAS2.csv'}
+              #'TELONAS2':   'https://data.pmel.noaa.gov/engineering/erddap/tabledap/prawler_eng_TELONAS2.csv'}
+              }
+
+rudics_vars = [
+            {'label':   'current location (in meters)', 'value':    'MM'},
+            {'label':   'current direction',            'value':    'DD'},
+            {'label':   'Error Counter',                'value':    'EC'},
+            {'label':   'Error timeout',                'value':    'ET'},
+            {'label':   'Trip number',                  'value':    'TS'},
+            {'label':   'Vacuum',                       'value':    'VM'},
+            {'label':   'Park timer',                   'value':    'PT'},
+            {'label':   'Water detection flag',         'value':    'WD'},
+            {'label':   'Climb down mode',              'value':    'CM'}
+            ]
 
 
 def gen_urls(base):
@@ -148,33 +165,45 @@ class current_data:
         self.fail_state = self.eng_data[self.eng_data['DD'] == 'F']
 
         if not self.fail_state.empty:
-        #     self.fail_state = False
-        # else:
-           self.fail_state.loc[:, 'tdiff'] = (self.fail_state.loc[:, 'datetime'].diff()).astype('timedelta64[m]')
+
+            self.fail_state.loc[:, 'tdiff'] = (self.fail_state.loc[:, 'datetime'].diff()).astype('timedelta64[m]')
+            self.fail_state.loc[:, 'ftrips'] = (self.fail_state.loc[:, 'TS'].diff())
+            self.fail_state.loc[:, 'dfx'] = (self.fail_state.loc[:, 'TS'].diff()) * abs(
+                self.eng_data.loc[0, 'LL'] - self.eng_data.loc[0, 'UL'])
 
         self.error_state = self.eng_data[self.eng_data.loc[:, 'EC'].diff() != 0]
 
         if not self.error_state.empty:
-        #     self.error_state = False
-        # else:
+
             self.error_state.loc[:, 'tdiff'] = (self.error_state.loc[:, 'datetime'].diff()).astype('timedelta64[m]')
+            self.error_state.loc[:, 'etrips'] = (self.error_state.loc[:, 'TS'].diff())
+            self.fail_state.loc[:, 'dex'] = (self.fail_state.loc[:, 'TS'].diff()) * abs(
+                self.eng_data.loc[0, 'LL'] - self.eng_data.loc[0, 'UL'])
+
 
         return self.error_state, self.fail_state
 
     def gen_vars(self):
 
-        self.vars = []
+        self.vars = rudics_vars
 
         if not self.fail_state.empty:
-            self.vars.append({'label': 'Failures', 'value': 'DD'})
             self.vars.append({'label': 'Time to Failure', 'value': 'TtFail'})
+            self.vars.append({'label': 'Trips to Failure', 'value': 'ftrips'})
+            self.vars.append({'label': 'Distance to Failure', 'value': 'dfx'})
+
 
         if not self.error_state.empty:
-            self.vars.append({'label': 'Errors', 'value': 'EC'})
             self.vars.append({'label': 'Time to Error', 'value': 'TtErr'})
+            self.vars.append({'label': 'Trips to Error', 'value': 'etrips'})
+            self.vars.append({'label': 'Distance to Error', 'value': 'dex'})
 
         return self.vars
 
+'''
+========================================================================================================================
+Dash App
+'''
 
 external_stylesheets = ['https://codepen.io./chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__)
@@ -189,7 +218,9 @@ graph_config = {'modeBarButtonsToRemove' : ['hoverCompareCartesian','select2d', 
                 'doubleClick':  'reset+autosize', 'toImageButtonOptions': { 'height': None, 'width': None, },
                 'displaylogo': False}
 
-app.layout =  ddk.App([
+colors = {'background': '#111111','text': '#7FDBFF'}
+
+app.layout = ddk.App([
     ddk.Header([
         ddk.Logo(src=app.get_asset_url('logo.png'), style={
             'max-height':100,
@@ -197,17 +228,21 @@ app.layout =  ddk.App([
         }),
         ddk.Title('Prawler Engineering Diagnostic Dashboard'),
         ddk.SectionTitle('', id='final_date'),
-        dhtml.Button('Refresh', style={'float':'right'}, id='refresh', n_clicks=0),
+        dhtml.Button('Refresh', style={'float': 'right'}, id='refresh', n_clicks=0),
     ]),
-dhtml.Div([
-    dhtml.Div([
+dhtml.Div(style={'backgroundColor': colors['background']},
+          children=[
+    dhtml.Div(style={'backgroundColor': colors['background']},
+              children=[
         ddk.Card(width=66,
+                 style={'backgroundColor': colors['background']},
                  children=[dcc.Loading(id='load_loader',
                                  children=[ddk.Graph(id='eng-graphic')]
                                        ),
                             dhtml.Label(['Prawler ID']),
                             dcc.Dropdown(
                                 id="select_set",
+                                style={'backgroundColor': colors['background']},
                                 options=prawlers,
                                 value=prawlers[0]['value'],
                                 clearable=False
@@ -215,6 +250,8 @@ dhtml.Div([
                             dhtml.Label(['Engineering Variables']),
                             dcc.Dropdown(
                                 id="select_var",
+                                style={'backgroundColor': colors['background'],
+                                       'textColor': colors['text']},
                                 options=var_list,
                                 value=var_list[0]['value'],
                                 clearable=False
@@ -222,12 +259,16 @@ dhtml.Div([
                 ],
             ),
         ddk.Card(width=34,
+                 style={'backgroundColor': colors['background']},
                  children=[dcc.Textarea(id='t_mean',
                                         value='',
-                                        style={'width': '100%', 'height': 40},
+                                        style={'width': '100%', 'height': 40,
+                                               'backgroundColor': colors['background']},
                                         ),
-                            dash_table.DataTable(id='table')])
-
+                            dash_table.DataTable(id='table',
+                                                 style_table={'backgroundColor': colors['background']},
+                                                 style_cell={'backgroundColor': colors['background']})
+                           ])
     ]),
     ddk.Card(children=[
         ddk.Block(width=6, children=
@@ -262,6 +303,11 @@ dhtml.Div([
 
 ])
 
+'''
+========================================================================================================================
+Callbacks
+'''
+
 # scientific data selection
 @app.callback(
     [Output('eng-graphic', 'figure'),
@@ -279,6 +325,8 @@ def plot_diag(select_set, select_var):
     cur_set = current_data(select_set)
     data = cur_set.eng_data
 
+    t_mean = ''
+
     if select_var == 'TtFail':
         figure = px.scatter(cur_set.fail_state,
                             y=cur_set.fail_state.loc[:, 'tdiff'],#.astype('timedelta64[m]'),
@@ -291,47 +339,72 @@ def plot_diag(select_set, select_var):
 
         figure.update_layout(
                             xaxis_title = "Time to Failure (minutes)",
-                            yaxis_title = "Date"
+                            yaxis_title = "Date",
+                            template='plotly_dark'
                             )
     elif select_var == 'TtErr':
         figure = px.scatter(cur_set.err_state,
                             y=cur_set.err_state.loc[:, 'tdiff'],#.astype('timedelta64[m]'),
                             x=cur_set.err_state.loc[:, 'datetime'])
         t_mean = 'Mean time to error: ' + str(round(cur_set.err_state.loc[:, 'tdiff'].mean())) + ' minutes'
-        table_data = cur_set.err_state.iloc[1:].to_dict('records')
+        table_data = cur_set.error_state.iloc[1:].to_dict('records')
         columns = [{"name": 'Date', "id": 'datetime'},
                    {'name': 'Time to Error (minutes)', 'id': 'tdiff'}]
         figure.update_layout(
-                            yaxis_title = "Time to Error (minutes)",
-                            xaxis_title = "Date"
+                            yaxis_title="Time to Error (minutes)",
+                            xaxis_title="Date",
+                            template='plotly_dark'
                             )
     elif select_var == 'EC':
         figure = px.scatter(cur_set.eng_data,
                             y=cur_set.eng_data.loc[:, select_var],
                             x=cur_set.eng_data.loc[:, 'datetime']
                             )
-        t_mean = 'Mean time to error: ' + str(round(cur_set.err_state.loc[:, 'tdiff'].mean())) + ' minutes'
-        table_data = cur_set.err_state.iloc[1:].to_dict('records')
+        if not cur_set.err_state.empty:
+            t_mean = 'Mean time to error: ' + str(round(cur_set.err_state.loc[:, 'tdiff'].mean())) + ' minutes'
+        table_data = cur_set.eng_data.iloc[1:].to_dict('records')
         columns = [{"name": 'Date', "id": 'datetime'},
-                   {'name': 'Time to Error (minutes)', 'id': 'tdiff'}]
+                   {'name': 'Error Count', 'id': 'EC'}]
         figure.update_layout(xaxis_title = "Date",
-                             yaxis_title = 'Error #')
+                             yaxis_title = 'Error #',
+                             template='plotly_dark'
+                             )
     elif select_var == 'DD':
         figure = px.scatter(cur_set.eng_data,
                             y=cur_set.eng_data.loc[:, select_var],
                             x=cur_set.eng_data.loc[:, 'datetime']
                             )
-        t_mean = 'Mean time to fail: ' + str(round(cur_set.fail_state.loc[:, 'tdiff'].mean())) + ' minutes'
-        table_data = cur_set.fail_state.iloc[1:].to_dict('records')
+        if not cur_set.fail_state.empty:
+            t_mean = 'Mean time to fail: ' + str(round(cur_set.fail_state.loc[:, 'tdiff'].mean())) + ' minutes'
+        table_data = cur_set.eng_data.to_dict('records')
         columns = [{"name": 'Date', "id": 'datetime'},
-                   {'name': 'Time to Failure (minutes)', 'id': 'tdiff'}]
-        figure.update_layout(xaxis_title = "Date",
-                             yaxis_title = 'Failure')
+                   {'name': 'Direction', 'id': 'DD'}]
+        figure.update_layout(xaxis_title="Date",
+                             yaxis_title='State',
+                             template='plotly_dark'
+                             )
+    else:
+        figure = px.scatter(cur_set.eng_data,
+                            y=cur_set.eng_data.loc[:, select_var],
+                            x=cur_set.eng_data.loc[:, 'datetime']
+                            )
+        t_mean = ''
+        table_data = cur_set.eng_data.to_dict('records')
+        columns = [{"name": 'Date', "id": 'datetime'},
+                   {'name': select_var, 'id': select_var}]
+        figure.update_layout(xaxis_title="Date",
+                             yaxis_title=select_var,
+                             template='plotly_dark'
+                             )
 
+    figure.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
 
     return figure, cur_set.vars, table_data, columns, t_mean
 
 if __name__ == '__main__':
     #app.run_server(host='0.0.0.0', port=8050, debug=True)
-
     app.run_server(debug=True)
